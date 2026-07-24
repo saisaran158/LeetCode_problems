b96aca1b383acd4ae726a7086bc2139aea64cdad
saisaran158 <saisaran158@gmail.com>
2026-07-24 12:21:33 +0530

    Time: 0 ms (100%), Space: 15.3 MB (36.37%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,18 +1,5 @@
 class Solution {
 public:
-    int binarySearch(int left, int right, vector<int> &nums, int target){
-        int mid;
-        while(left <= right){
-            mid = left + (right - left) / 2;
-            if(nums[mid] == target)
-                return mid;
-            else if(nums[mid] < target)
-                left = mid + 1;
-            else 
-                right = mid - 1;
-        }
-        return -1;
-    }
     int search(vector<int>& nums, int target) {
         int left = 0, right = nums.size() - 1 ;
         while(left <= right){
@@ -20,15 +7,21 @@ public:
             if(nums[mid] == target){
                 return mid;
             }
-            else if(nums[left] < nums[mid]){
-                int c = binarySearch(left, mid -1, nums, target);
-                if(c == -1)left = mid + 1;
-                else return c;
+            else if(nums[left] <= nums[mid]){
+                if(nums[left] <= target && target <= nums[mid]){
+                    right = mid - 1;
+                }
+                else{
+                    left = mid + 1;
+                }
             }
             else{
-                int f = binarySearch(mid + 1, right, nums, target);
-                if(f == -1)right = mid - 1;
-                else return f;
+                if(nums[mid] <= target && target <= nums[right]){
+                    left = mid + 1;
+                }
+                else{
+                    right = mid - 1;
+                }
             }
         }
         return -1;
