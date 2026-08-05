dca5a7f5f1a7cfaa83f047ede412dc358bf60b2b
saisaran158 <saisaran158@gmail.com>
2026-08-05 22:22:39 +0530

    Time: 0 ms (100%), Space: 10.2 MB (79.2%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    bool check(int row, int col, vector<string>& board, int n){
+        int duprow = row;
+        int dupcol = col;
+
+        while(row >= 0 && col >= 0){
+            if(board[row][col] == 'Q') return false;
+            row--;
+            col--;
+        }
+
+        row = duprow;
+        col = dupcol;
+
+        while(dupcol >= 0){
+            if(board[row][dupcol] == 'Q') return false;
+            dupcol--;
+        }
+
+        duprow = row;
+        dupcol = col;
+
+        while(row < n && col >= 0){
+            if(board[row][col] == 'Q') return false;
+            row++;
+            col--;
+        }
+
+        return true;
+    }
+    void solve(int col, vector<string>& board, vector<vector<string>>& ans, int n){
+        if(col == n){
+            ans.push_back(board);
+            return;
+        }
+
+        for(int row = 0; row < n; row++){
+            if(check(row, col, board, n)){
+                board[row][col] = 'Q';
+                solve(col + 1, board, ans, n);
+                board[row][col] = '.';
+            }
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        string s(n, '.');
+        vector<string> board(n, s);
+        solve(0, board, ans, n);
+        return ans;
+    }
+};
