2e6503d52778db4c7925366549107e225db06ed9
saisaran158 <saisaran158@gmail.com>
2026-08-10 12:20:17 +0530

    Time: 1879 ms (5.13%), Space: 398.1 MB (6.41%) - LeetHub

---
+++ 2641-cousins-in-binary-tree-ii/2641-cousins-in-binary-tree-ii.cpp
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int recursion(TreeNode* root, map<TreeNode*, int>&sib){
+        if(!root) return 0;
+
+        int left = recursion(root -> left, sib);
+        int right = recursion(root -> right, sib);
+        if(root -> left)
+        sib[root -> left] = left + right;
+        if(root -> right)
+        sib[root -> right] = left + right;
+        return root -> val;
+    }
+    void recursions(TreeNode* root, vector<int>& level, int lev){
+        if(!root) return ;
+        
+        if(level.size() == lev){
+            level.push_back(root -> val);
+        }
+        else
+        level[lev] += root -> val;
+        recursions(root -> left, level, lev + 1);
+        recursions(root -> right, level, lev + 1);
+    }
+    void solve(TreeNode* root, int lev, map<TreeNode*, int>& sib, vector<int>& level){
+        if(!root) return;
+
+        root -> val = level[lev] - sib[root];
+        solve(root -> left, lev + 1, sib, level);
+        solve(root -> right, lev + 1, sib, level);
+    }
+    TreeNode* replaceValueInTree(TreeNode* root) {
+        map<TreeNode*, int>sib;
+        recursion(root, sib);
+        vector<int> level;
+        recursions(root, level, 0);
+        solve(root, 0, sib, level);
+        root -> val = 0;
+        return root;
+    }
+};
