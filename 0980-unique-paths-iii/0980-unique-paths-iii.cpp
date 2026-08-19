a5077db7c70f085cb3d05f442d325353becd6c19
saisaran158 <saisaran158@gmail.com>
2026-08-19 16:07:13 +0530

    Time: 19 ms (8.08%), Space: 9.4 MB (45.52%) - LeetHub

--- 0980-unique-paths-iii/0980-unique-paths-iii.cpp
+++ 0980-unique-paths-iii/0980-unique-paths-iii.cpp
@@ -3,12 +3,12 @@ public:
     void recursion(int sr, int sc, int er, int ec, vector<vector<int>>& grid, int count, int& ans, vector<vector<int>>& vis){
         if(sr == er && sc == ec){
             cout << sr << "-" << sc << "-" << count << " ";
-            if(count  == 1){
+            if(count == 0){
                 ans++;
             }
             return;
         }
-        vis[sr][sc]=1;
+        vis[sr][sc] = 1;
         int m = grid.size();
         int n = grid[0].size();
         int delr[] = {-1, 0, 1, 0};
@@ -20,13 +20,14 @@ public:
 
             if(nr >= 0 && nc >= 0 && nr < m && nc < n){
                 if(grid[nr][nc] != -1 && vis[nr][nc] == 0){
-        
-                    recursion(nr, nc, er, ec, grid, count - 1, ans, vis);
-    
+                    vis[nr][nc] = 1;
+                    count--;
+                    recursion(nr, nc, er, ec, grid, count, ans, vis);
+                    count++;
+                    vis[nr][nc] = 0;
                 }
             }
         }
-         vis[sr][sc]=0;
     }
     int uniquePathsIII(vector<vector<int>>& grid) {
         int ans = 0;
@@ -48,7 +49,7 @@ public:
             }
         }
         vector<vector<int>>vis(grid.size(), vector<int>(grid[0].size(), 0));
-        recursion(sr, sc, er, ec, grid, count - c, ans, vis);
+        recursion(sr, sc, er, ec, grid, count - 1 - c, ans, vis);
         return ans;
     }
 };
