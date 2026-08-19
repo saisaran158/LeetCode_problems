cfdff77378f54bdf4028a9b6a230f197a9e5a12d
saisaran158 <saisaran158@gmail.com>
2026-08-19 14:05:31 +0530

    Time: 58 ms (5.33%), Space: 35.6 MB (5.14%) - LeetHub

---
+++ 0174-dungeon-game/0174-dungeon-game.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    bool bfs(vector<vector<int>>& dungeon, int hhp) {
+        if (hhp + dungeon[0][0] < 1)
+            return false;
+        int m = dungeon.size();
+        int n = dungeon[0].size();
+        vector<vector<int>> vis(m, vector<int>(n, -1));
+        queue<pair<int, pair<int, int>>> q;
+        q.push({hhp + dungeon[0][0], {0, 0}});
+        vis[0][0] = hhp + dungeon[0][0];
+        int delr[] = {0, 1};
+        int delc[] = {1, 0};
+        while (!q.empty()) {
+            int hp = q.front().first;
+            int r = q.front().second.first;
+            int c = q.front().second.second;
+            q.pop();
+            if (r == m - 1 && c == n - 1)
+                return true;
+            if(vis[r][c] > hp) continue;
+            for (int i = 0; i < 2; i++) {
+                int nr = r + delr[i];
+                int nc = c + delc[i];
+
+                if (nr >= 0 && nc >= 0 && nr < m && nc < n) {
+                    if (hp + dungeon[nr][nc] >= 1 &&
+                        vis[nr][nc] < hp + dungeon[nr][nc]) {
+                        q.push({hp + dungeon[nr][nc], {nr, nc}});
+                        vis[nr][nc] = hp + dungeon[nr][nc];
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    int calculateMinimumHP(vector<vector<int>>& dungeon) {
+        int low = 1;
+        int high = 1e9;
+        int ans = INT_MIN;
+        int m = dungeon.size();
+        int n = dungeon[0].size();
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (bfs(dungeon, mid)) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
