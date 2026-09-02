fa76a81b64973687c189bf3d8118a3dcf76ba441
saisaran158 <saisaran158@gmail.com>
2026-09-02 11:02:38 +0530

    Time: 0 ms (100%), Space: 8.2 MB (63.81%) - LeetHub

--- 3857-minimum-cost-to-split-into-ones/3857-minimum-cost-to-split-into-ones.cpp
+++ 3857-minimum-cost-to-split-into-ones/3857-minimum-cost-to-split-into-ones.cpp
@@ -1,7 +1,14 @@
 class Solution {
 public:
+    int recursion(int n){
+        if(n == 1) return 0;
+
+        int cost = 0;
+        cost = n - 1;
+        cost += recursion(n - 1); 
+        return cost;
+    }
     int minCost(int n) {
-        int c = n - 1;
-        return c*(c+1)/2;
+        return recursion(n);
     }
 };
