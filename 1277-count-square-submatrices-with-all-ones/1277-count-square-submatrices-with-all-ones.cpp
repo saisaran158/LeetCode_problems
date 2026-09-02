e7fe8abf2b34cf0885b697a7948e9147a759f158
saisaran158 <saisaran158@gmail.com>
2026-09-02 13:29:28 +0530

    Time: 18 ms (12.15%), Space: 30.3 MB (49.63%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
@@ -18,7 +18,7 @@ public:
         for(int i = 1; i <= m; i++){
             for(int j = 1; j <= n; j++){
                 int m = min({dp[i][j - 1], dp[i - 1][j - 1], dp[i - 1][j]});
-                if(dp[i][j] == 1)
+                if(dp[i][j] != 0)
                 dp[i][j] = m >= 1 ? m + 1 : dp[i][j];
             }
         }
