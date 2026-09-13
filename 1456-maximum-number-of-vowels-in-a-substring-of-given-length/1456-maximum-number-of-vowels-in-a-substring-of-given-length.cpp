deab512de1ee0cf7463f9923436c5709e27faf5c
saisaran158 <saisaran158@gmail.com>
2026-09-13 23:15:20 +0530

    Time: 0 ms (100%), Space: 13.1 MB (58.74%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -7,17 +7,17 @@ public:
                 c++;
             }
         }
-        int a = c;
+        int maxi = c;
         for(int i = k; i < s.size(); i++){
             if(s[i - k] == 'a' || s[i - k] == 'e' || s[i - k] == 'i' || s[i - k] == 'o' || s[i - k] == 'u'){
-                a--;
+                c--;
             }
             if(s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' ||s[i] == 'u'){
-                a++;
+                c++;
             }
-            c = max(c, a);
+            maxi = max(c, maxi);
             
         }
-        return c;
+        return maxi;
     }
 };
